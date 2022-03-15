87b86bcd775abded84c4cbd418292e974937fc68
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-03-15 14:03:10 -0300

    Corrigido Lint, funçao refatorada

--- src/restaurant.js
+++ src/restaurant.js
@@ -82,38 +82,21 @@
 let restaurant = {};
 const orderFromMenu = (request) => restaurant.consumption.push(request);
 
-// const somaFood = () => {
-//   const menuFood = Object.keys(restaurant.fetchMenu().food);
-//   const valorFood = Object.values(restaurant.fetchMenu().food);
-//   let valoresMenu = 0;
-//   for (let i = 0; i < menuFood.length; i += 1) {
-//     for (let i2 = 0; i2 < restaurant.consumption.length; i2 += 1) {
-//       if (menuFood[i] === restaurant.consumption[i2]) {
-//         valoresMenu += valorFood[i] * 1.1;
-//       }
-//     }
-//   }
-//   return valoresMenu;
-// };
+const comparaDrink = (consumoDrink, valorDrinks, menu) => {
+  let valoresMenu = 0;
+  if (consumoDrink === menu) {
+    valoresMenu += valorDrinks;
+  }
+  return valoresMenu;
+};
 
-// const somaDrink = () => {
-//   const menuDrinks = Object.keys(restaurant.fetchMenu().drinks);
-//   const valorDrinks = Object.values(restaurant.fetchMenu().drinks);
-//   let valoresMenu = 0;
-//   for (let i = 0; i < menuFood.length; i += 1) {
-//     for (let i2 = 0; i2 < restaurant.consumption.length; i2 += 1) {
-//       if (menuDrinks[i] === restaurant.consumption[i2]) {
-//         valoresMenu += valorDrinks[i] * 1.1;
-//       }
-//     }
-//   }
-//   return valoresMenu;
-// };
-
-// const somaValores = () => {
-//   const valorTotal =  somaFood() + somaDrink();
-//   return valorTotal;
-// };
+const comparaFood = (consumoFood, valorFood, menu) => {
+  let valoresMenu = 0;
+  if (consumoFood === menu) {
+    valoresMenu += valorFood;
+  }
+  return valoresMenu;
+};
 
 const somaValores = () => {
   const menuFood = Object.keys(restaurant.fetchMenu().food);
@@ -123,14 +106,11 @@ const somaValores = () => {
   let valoresMenu = 0;
   for (let i = 0; i < menuFood.length; i += 1) {
     for (let i2 = 0; i2 < restaurant.consumption.length; i2 += 1) {
-      if (menuFood[i] === restaurant.consumption[i2]) {
-        valoresMenu += valorFood[i] * 1.1;
-      } else if (menuDrinks[i] === restaurant.consumption[i2]) {
-        valoresMenu += valorDrinks[i] * 1.1;
-      }
+     valoresMenu += comparaFood(menuFood[i], valorFood[i], restaurant.consumption[i2]);
+     valoresMenu += comparaDrink(menuDrinks[i], valorDrinks[i], restaurant.consumption[i2]);
     }
   }
-  return valoresMenu;
+  return valoresMenu * 1.1;
 };
 
 const createMenu = (object) => {
